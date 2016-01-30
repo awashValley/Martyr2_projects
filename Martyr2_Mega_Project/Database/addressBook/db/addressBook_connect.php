77f7bb7ddf3a6be5671d5f6fa675ba4b836f4e94
awashValley <hammerson1@gmail.com>
2016-01-30 14:41:04 +0100

    Update addressBook_connect.php

--- Martyr2_Mega_Project/Database/addressBook/db/addressBook_connect.php
+++ Martyr2_Mega_Project/Database/addressBook/db/addressBook_connect.php
@@ -1,13 +1,13 @@
 <?php
   // make connection
-	// $con = mysqli_connect("localhost", "root", "password", "Martyr2_db_addressBook");
+  // $con = mysqli_connect("localhost", "root", "password", "Martyr2_db_addressBook");
   $con = mysql_connect("localhost", "root", "password");
 
-	// define database
-  mysql_select_db("Martyr2_db_addressBook", $con);
+ // define database
+ mysql_select_db("Martyr2_db_addressBook", $con);
 
-	if(!($con)){
-		die("The connection to database was not successful.");
-	}
+ if(!($con)){
+   die("The connection to database was not successful.");
+ }
 
 ?>
